7591a9b9c0d17bf50dccd097044a72ae2a2b92cc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-12-16 23:30:29 +0900

    Add currentUser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from 'next';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 import { appwrite } from '../appwrite';
+import { currentUserState } from '../atoms';
 
 const Index: NextPage = () => {
+  const currentUser = useRecoilValue(currentUserState);
+
   const handleClick = async () => {
     await appwrite.account.createOAuth2Session('github', 'http://localhost:3000/path', 'http://localhost:3000/path');
   };
 
-  return (
-    <div>
-      <button onClick={handleClick}>ログイン</button>
-    </div>
-  );
+  return <div>{currentUser ? <div>ログイン中</div> : <button onClick={handleClick}>ログイン</button>}</div>;
 };
 
 export default Index;
