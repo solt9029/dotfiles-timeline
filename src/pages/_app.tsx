8721e83a83ee73136bedf07cf828340803d9213d
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-12-19 20:45:02 +0900

    Add return

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
-import { RecoilRoot, useSetRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
 import { appwrite } from '../appwrite';
 import { currentUserState } from '../atoms/current-user';
 
 const AppInit = () => {
-  const setCurrentUser = useSetRecoilState(currentUserState);
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
 
   useEffect(() => {
     (async () => {
+      if (currentUser) {
+        return;
+      }
+
       try {
         const session = await appwrite.account.getSession('current');
         setCurrentUser({ id: session.$id, providerToken: session.providerToken });
@@ -17,7 +21,7 @@ const AppInit = () => {
         setCurrentUser(undefined);
       }
     })();
-  }, [setCurrentUser]);
+  }, [currentUser, setCurrentUser]);
 
   return null;
 };
