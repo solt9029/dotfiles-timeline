6e2c0747e7c11afd84eba49b55dce6b267846dc0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-12-21 00:55:27 +0900

    Add updateCommits function

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { fetchCommits, fetchFollowingUsers } from '../api-clients/github';
 import { currentUserState } from '../atoms/current-user';
@@ -12,44 +12,46 @@ const Timeline: NextPage = () => {
   const currentUser = useRecoilValue(currentUserState);
   const [githubFollowingUsers, setGithubFollowingUsers] = useRecoilState(githubFollowingUsersState);
 
-  useEffect(() => {
-    (async () => {
-      if (githubFollowingUsers === undefined || currentUser === undefined) {
-        return;
-      }
+  const updateCommits = useCallback(async () => {
+    if (githubFollowingUsers === undefined || currentUser === undefined) {
+      return;
+    }
 
-      let newGithubFollowingUsers = clone(githubFollowingUsers);
+    let newGithubFollowingUsers = clone(githubFollowingUsers);
 
-      const index = newGithubFollowingUsers.findIndex(
-        ({ updatedAt }) => updatedAt == undefined || dayjs().isAfter(dayjs(updatedAt).add(1, 'd'))
-      );
-      if (index === -1) {
-        return;
-      }
+    const index = newGithubFollowingUsers.findIndex(
+      ({ updatedAt }) => updatedAt == undefined || dayjs().isAfter(dayjs(updatedAt).add(100, 'second'))
+    );
+    if (index === -1) {
+      return;
+    }
 
-      try {
-        const res = await fetchCommits(newGithubFollowingUsers[index].login, currentUser.providerToken);
+    try {
+      const res = await fetchCommits(newGithubFollowingUsers[index].login, currentUser.providerToken);
 
-        newGithubFollowingUsers[index].commits = (res?.data || []).map(({ commit, committer, html_url }) => {
-          return {
-            message: commit.message,
-            date: commit.committer.date,
-            commentCount: commit.comment_count,
-            committer: {
-              login: committer?.login,
-              avatarUrl: committer?.avatar_url,
-            },
-            htmlUrl: html_url,
-          };
-        });
-        newGithubFollowingUsers[index].updatedAt = dayjs().toDate();
+      newGithubFollowingUsers[index].commits = (res?.data || []).map(({ commit, committer, html_url }) => {
+        return {
+          message: commit.message,
+          date: commit.committer.date,
+          commentCount: commit.comment_count,
+          committer: {
+            login: committer?.login,
+            avatarUrl: committer?.avatar_url,
+          },
+          htmlUrl: html_url,
+        };
+      });
+      newGithubFollowingUsers[index].updatedAt = dayjs().toDate();
 
-        setGithubFollowingUsers(newGithubFollowingUsers);
-      } catch (err) {
-        console.log(err);
-      }
-    })();
+      setGithubFollowingUsers(newGithubFollowingUsers);
+    } catch (err) {
+      console.log(err);
+    }
   }, [currentUser, githubFollowingUsers, setGithubFollowingUsers]);
+
+  useEffect(() => {
+    updateCommits();
+  }, [updateCommits]);
 
   return <div>timeline</div>;
 };
