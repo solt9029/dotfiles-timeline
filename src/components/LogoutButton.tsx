b9286197e638ea9c27dd7aaeb009472c5e784dd9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-12-21 23:12:03 +0900

    Add push

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,13 +3,16 @@ import { useResetRecoilState } from 'recoil';
 import { appwrite } from '../appwrite';
 import { state } from '../atoms';
 import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/dist/client/router';
 
 export const LogoutButton = () => {
   const resetState = useResetRecoilState(state);
+  const router = useRouter();
 
   const handleClick = async () => {
     await appwrite.account.deleteSession('current');
     resetState();
+    router.push('/');
   };
 
   return (
