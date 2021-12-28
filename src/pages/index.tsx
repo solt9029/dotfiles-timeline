cbd150e2e4f470153f7ee0755ebe2e693dc70ce5
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-12-28 22:43:19 +0900

    Add screenshot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { AppNavbar } from '../components/AppNavbar';
 import { Jumbotron } from '../components/Jumbotron';
 import { Footer } from '../components/Footer';
+import Image from 'next/image';
 
 const Index: NextPage = () => {
   return (
@@ -19,9 +20,16 @@ const Index: NextPage = () => {
             <h2>『dotfiles-timeline』</h2>
           </div>
 
-          <div>
+          <div className="mb-5">
             『dotfiles-timeline』はGitHub上でフォローしているユーザのdotfilesのコミットを時系列順に見ることのできるサービスです。
           </div>
+
+          <Row>
+            <Col md={4}></Col>
+            <Col md={4}>
+              <Image src="/screenshot.png" alt="スクリーンショット" width={100} height={143} layout="responsive" />
+            </Col>
+          </Row>
         </Container>
       </div>
       <div className="mt-5">
